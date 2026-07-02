d02b68e1f75f7ecb2ee8d19c96e4ece7ebe9699d
Melii <melinfikri129@gmail.com>
2026-07-02 12:39:09 +0300

    Add Create menu

--- Program.cs
+++ Program.cs
@@ -27,6 +27,27 @@ namespace FirstProject1
 
                 string choice = Console.ReadLine();
                 Console.WriteLine();
+                switch (choice)
+                {
+                    case "1":
+                        //1.Create
+                        Console.Write("Въведете име: ");
+                        string name = Console.ReadLine();
+
+                        Console.Write("Въведете възраст: ");
+                        int age = int.Parse(Console.ReadLine());
+
+                        Console.Write("Въведете заплата: ");
+                        double salary = double.Parse(Console.ReadLine());
+
+                        Person newPerson = new Person(name, age, salary);
+                        people.Add(newPerson);
+
+                        SavePeopleToFile(people);
+                        Console.WriteLine("Успешно добавен нов запис!");
+                        Console.WriteLine();
+                        break;
+                }
             }
         }
         static void SavePeopleToFile(List<Person> people)
